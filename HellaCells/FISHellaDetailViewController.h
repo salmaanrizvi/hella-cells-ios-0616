513ac9a2976d99f474736a744e1a6947118fd372
salmaanrizvi <sar228@cornell.edu>
2016-06-21 19:41:09 -0400

    all tests passed

---
+++ HellaCells/FISHellaDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  FISHellaDetailViewController.h
+//  HellaCells
+//
+//  Created by Salmaan Rizvi on 6/21/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISHellaDetailViewController : UIViewController
+
+@property (nonatomic, strong) NSString *numberString;
+
+@end
---
+++ HellaCells/TableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableTableViewController.h
+//  HellaCells
+//
+//  Created by Salmaan Rizvi on 6/21/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableTableViewController : UITableViewController
+
+@end
